7940ad8a17fb0ce6341b8a2fd4bcdc1334cdd7a3
sirageDb <ilovesemicolons@hotmail.com>
2022-11-08 08:25:36 +0100

    UPDATE: environment variables

--- src/config/env.dev.ts
+++ src/config/env.dev.ts
@@ -14,7 +14,7 @@ const options:TOptions = {
 
 
 
-const mongoProd = process.env.DB_CONNECTION_STRING || '';
+const mongoProd = process.env.WIKINOTES_DB_TEST_URL || '';
 const mongoDev = "mongodb://localhost:27017/wiki-notes";
 const dbConnection = () : string => {
   if(process.env.NODE_ENV === 'production'){
@@ -39,7 +39,7 @@ const config: IConfig = {
   serverPort: Number(process.env.PORT) || 5000,
   serverStart: true,
   verbose: true,
-  token: process.env.JWT_KEY || 'notsecure',
+  token: process.env.WIKINOTES_TOKEN_SECRET || 'notsecure',
 };
 
 export default config;
